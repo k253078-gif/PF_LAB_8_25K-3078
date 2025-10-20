a0da783c628ff02e28654363b0f211cbfd8d758f
Muhammad Wasi Bin Tariq <k253078@nu.edu.pk>
2025-10-20 15:44:52 +0500

    Uploaded lab tasks

---
+++ Task_2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	
+    int a,n;
+      
+    printf("Enter a your number: ");
+    scanf("%d",&a);
+    
+    for(int i=0; i <= 10; i++){
+    	
+    	printf("%d x %d is %d \n", a ,i ,n=a*i);
+	}
+	return 0;
+}
